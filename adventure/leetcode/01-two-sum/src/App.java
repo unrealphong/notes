5a6085af224713bd144d1c37d2bf054265df3bc7
unrealphong <hongphongle115@gmail.com>
2024-08-06 18:11:24 +0700

    started leetcode

---
+++ adventure/leetcode/01-two-sum/src/App.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+
+        if (result.length > 0) {
+            System.out.println("Indices: " + result[0] + " and " + result[1]);
+        } else {
+            System.out.println("No two sum solution");
+        }
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] {map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0]; 
+    }
+}
